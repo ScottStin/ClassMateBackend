4b82346b1856617b7ca36fb6fb20357f948414ca
Scott Stinson <sstinson@nltdigital.com>
2023-09-03 12:14:24 +0200

    signup and auth routes done. login bug fix

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -4,9 +4,9 @@ const multer = require('multer');
 const { cloudinary, storage } = require('../cloudinary');
 const upload = multer({ storage });
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 
 const userModel = require('../models/user-models');
-// const examModel = require('../models/exam-model.js');
 
 router.get('/', async (req, res) => {
   try {
@@ -20,15 +20,36 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
   try {
-    const newUser = new userModel(req.body);
-    newUser.hashedPassword = await bcrypt.hash(newUser.unhashedPassword, 12 , async () => {
-      await newUser.save();    
-      res.status(200).send(newUser); 
-    })
+    const hashedPassword = await bcrypt.hash(req.body.unhashedPassword, 12)
+    const newUser = await new userModel(req.body);
+    newUser.hashedPassword = hashedPassword;
+    await newUser.save();
 
   } catch (error) {
     console.error("Error:", error);
     res.status(500).send("Internal Server Error");
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const user = await userModel.findOne({ email: req.body.email });
+    const validPassword = await bcrypt.compare(req.body.unhashedPassword, user.hashedPassword);
+
+    if (!user || !validPassword) {
+      return res.status(401).json({ error: 'Authentication failed. Invalid email or password.' });
+    }
+
+    const token = jwt.sign(
+      { userId: user._id, email: user.email },
+     'placeholderSecretKey', //process.env.JWT_SECRET, // Replace with secret key
+      { expiresIn: '12h' }
+    );
+    res.status(200).json({ token, userId: user._id, user });
+
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: 'Internal server error' });
   }
 });
 
