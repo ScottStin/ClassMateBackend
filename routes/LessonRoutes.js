44d27c0d5431c821725e95f3cec1f8a15ebd2cde
Scott Stinson <sstinson@nltdigital.com>
2023-11-15 13:46:02 +0100

    my classes page and logic done, join and leave class done

--- routes/LessonRoutes.js
+++ routes/LessonRoutes.js
@@ -25,6 +25,58 @@ router.post('/new', async (req, res) => {
   }
 });
 
+router.patch('/register/:id', async (req, res) => {
+  try {
+    const lesson = await lessonModel.findById(req.params.id);
+
+    if (!lesson) {
+      return res.status(404).json('Lesson not found');
+    }
+
+    const userEmail = req.body.email;
+
+    if (lesson.studentsEnrolled.includes(userEmail)) {
+      return res.status(400).json('User has already registered for this lesson');
+    }
+
+    lesson.studentsEnrolled.push(userEmail);
+    await lesson.save();
+
+    res.json(`Student added to: ${lesson}`);
+  } catch (error) {
+    console.error("Error join lessons:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.patch('/cancel/:id', async (req, res) => {
+  try {
+    const lesson = await lessonModel.findById(req.params.id);
+
+    if (!lesson) {
+      return res.status(404).json('Lesson not found');
+    }
+
+    const userEmail = req.body.email;
+
+    if (!lesson.studentsEnrolled.includes(userEmail)) {
+      return res.status(400).json('User is not currently enrolled in this lesson');
+    }
+    console.log(userEmail);
+    console.log(lesson.studentsEnrolled);
+    index = lesson.studentsEnrolled.indexOf(userEmail);
+    if (index > -1) {
+      lesson.studentsEnrolled.splice(index, 1);
+    }
+    await lesson.save();
+
+    res.json(`Student removed from: ${lesson}`);
+  } catch (error) {
+    console.error("Error leaving lessons:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 router.delete('/:id', async (req, res) => {
   try {
     const deletedLesson = await lessonModel.findByIdAndDelete(req.params.id);
