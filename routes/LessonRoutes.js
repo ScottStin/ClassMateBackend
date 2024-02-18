249e07a1e81635b4164349c4b03fcadd7eb0d2fb
Scott Stinson <sstinson@nltdigital.com>
2024-02-18 18:16:06 +0100

    add and remove students from class done

--- routes/LessonRoutes.js
+++ routes/LessonRoutes.js
@@ -39,10 +39,44 @@ router.patch('/register/:id', async (req, res) => {
       return res.status(400).json('User has already registered for this lesson');
     }
 
+    if(lesson.studentsEnrolled.length >= lesson.maxStudents) {
+      return res.status(400).json('Max students in lesson already reached');
+    }
+
     lesson.studentsEnrolled.push(userEmail);
     await lesson.save();
 
     res.json(`Student added to: ${lesson}`);
+  } catch (error) {
+    console.error("Error join lessons:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.patch('/register-multi/:id', async (req, res) => {
+  try {
+    const lesson = await lessonModel.findById(req.params.id);
+
+    if (!lesson) {
+      return res.status(404).json('Lesson not found');
+    }
+
+    // Remove students not in req.body from lesson.studentsEnrolled
+    lesson.studentsEnrolled = lesson.studentsEnrolled.filter(
+      (email) => req.body.some((student) => student.email === email)
+    );
+
+    // Add new students to lesson.studentsEnrolled
+    for (const student of req.body) {
+      userEmail = student.email
+      if (!lesson.studentsEnrolled.includes(userEmail) && lesson.studentsEnrolled.length < lesson.maxStudents) {
+        lesson.studentsEnrolled.push(userEmail);
+      }
+    }
+
+    await lesson.save();
+
+    res.json(`Students added to: ${lesson}`);
   } catch (error) {
     console.error("Error join lessons:", error);
     res.status(500).send("Internal Server Error");
